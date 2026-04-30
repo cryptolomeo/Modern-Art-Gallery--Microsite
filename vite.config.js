00e421eed862a8a6d638eb8c18e476d391d2e355
Ivan Rodriguez <taliantcode@gmail.com>
2026-04-30 11:07:28 +0200

    Boilerplate charged. Html+Sass with vite

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+// Maneja las rutas de los enlaces convirtiendolas en relativas
+import { defineConfig } from "vite";
+export default defineConfig({
+  base: "./",
+  /*
+    base: "/Wellness-Spa-Website/",     //ADD name repo
+    */
+});
