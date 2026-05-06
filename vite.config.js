39f32a3e7b00ecc73848ca670b07e6cac03c8d3c
Ivan Rodriguez <taliantcode@gmail.com>
2026-05-06 14:04:41 +0200

    Fix navigation links for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,5 @@
 // Maneja las rutas de los enlaces convirtiendolas en relativas
 import { defineConfig } from "vite";
 export default defineConfig({
-  base: "./",
-  /*
-    base: "/Wellness-Spa-Website/",     //ADD name repo
-    */
+  base: '/Modern-Art-Gallery--Microsite/',
 });
